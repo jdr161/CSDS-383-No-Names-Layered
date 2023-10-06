9d016c5aea9f195e14b1340fbb71cb5a92c0f7a3
James Redding <98284325+jdr161@users.noreply.github.com>
2023-10-05 22:35:49 -0400

    Jdr161/forms (#1)
    * remove extraneous import from forms
    
    * basic event form
    
    * CreateEventForm - add advanced input validation
    
    * awaiting api link
    
    * add create-participant and register-participant form
    
    * remove testing comments
    
    * fixed error message

--- presentation-layer/src/components/Forms/RegisterParticipantForm.js
+++ presentation-layer/src/components/Forms/RegisterParticipantForm.js
@@ -1,16 +1,72 @@
 import React, { Component } from "react"
-import { render } from "react-dom"
+import {
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    Input,
+    Button,
+} from '@chakra-ui/react'
+import axios from 'axios'
 
 class RegisterParticipantForm extends Component {
     constructor() {
         super()
-        this.state = {}
+        this.state = {
+            eventInput: '',
+            participantInput: '',
+        }
     }
+
+
     render() {
-        return(
+        const { eventInput, participantInput } = this.state
+        const handleEventChange = (e) => this.setState({ eventInput: e.target.value})
+        const handleParticipantChange = (e) => this.setState({ participantInput: e.target.value})
+
+        const isEventError = eventInput === ''
+        const isParticipantError = participantInput === ''
+
+        const submitDisabled = isEventError | isParticipantError
+
+        const handleSubmit = () => {
+            let data = {
+                event: eventInput,
+                Participant: participantInput,
+            }
+            let apiURL = ''
+            axios.post(apiURL, data)
+              .then(function (response) {
+                //TODO: IMPLEMENT API RESPONSE
+                console.log(response);
+              })
+              .catch(function (error) {
+                //TODO: IMPLEMENT API ERROR RESPONSE
+                console.log(error);
+              });
+        }
+
+        return (
+            <>
             <div>
-                RegisterParticipantForm
+                <FormControl isInvalid={isEventError}>
+                    <FormLabel>Event UUID</FormLabel>
+                    <Input type='text' value={eventInput} onChange={handleEventChange} placeholder="" />
+                    {isEventError &&
+                        <FormErrorMessage>Event UUID is required</FormErrorMessage>
+                    }
+                </FormControl>
+                
+                <FormControl isInvalid={isParticipantError}>
+                    <FormLabel>Participant UUID</FormLabel>
+                    <Input type='text' value={participantInput} onChange={handleParticipantChange} placeholder="" />
+                    {isParticipantError &&
+                        <FormErrorMessage>Participant UUID is required</FormErrorMessage>
+                    }
+                </FormControl>
+
+                <Button onClick={handleSubmit} isDisabled={submitDisabled}>Submit</Button>
             </div>
+            </>
         )
     }
 } export default RegisterParticipantForm
